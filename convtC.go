47a242af7c535ec52964b04bb8ba7a5688315478
FaizanMajid1 <73417391+FaizanMajid1@users.noreply.github.com>
2021-01-17 08:20:00 +0500

    updated to use command line arguments

--- convtC.go
+++ convtC.go
@@ -1,11 +1,15 @@
 //Converts degree Fahrenheit To degree celcius .
 package main
-import "fmt"
-     func main() {
-     	var f int
-     	fmt.Println("Enter temperature in Fahrenheit : ")
-     	fmt.Scanln(&f)
-        var c = (f - 32) * 5 / 9
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+func main() {
+
+    f, err := strconv.Atoi(os.Args[1])
+    if err == nil {
+        c := (f - 32) * 5 / 9
         fmt.Println("Temperature in Celcius is : ", c,"°C")
-  
+  	}
 }
