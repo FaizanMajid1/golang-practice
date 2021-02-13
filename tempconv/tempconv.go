aa392aa741a5557388f87f14e7c0f5a2a8200e4d
FaizanMajid1 <73417391+FaizanMajid1@users.noreply.github.com>
2021-02-13 22:24:06 +0500

    package tempconv

---
+++ tempconv/tempconv.go
@@ -0,0 +1,16 @@
+package tempconv
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+
+func CtoF(c Celsius) Fahrenheit {
+    return Fahrenheit(c*9/5 + 32)
+}
+
+func FtoC(f Fahrenheit) Celsius {
+    return Celsius((f-32)*5/9)
+}
+
+func (c Celsius) String() string { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
